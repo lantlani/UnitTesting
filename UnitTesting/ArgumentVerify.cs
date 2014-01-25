d91d03734d6b83cceb17cfade83ad91c00f56444
SEYOUNG PYO <lantlani@gmail.com>
2014-01-25 10:07:28 +0900

    Register Initial Codes.

---
+++ UnitTesting/ArgumentVerify.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Rhino.Mocks.Constraints;
+using Is = Rhino.Mocks.Constraints.Is;
+
+namespace UnitTesting
+{
+    class ArgumentVerify
+    {
+        [Test]
+        public void ComparePropertyExample()
+        {
+            MockRepository mocks = new MockRepository();
+            IWebService mockService = mocks.CreateMock<IWebService>();
+
+            using (mocks.Record())
+            {
+                mockService.LogError(new TraceMessage("", 0, ""));
+
+                LastCall.Constraints(
+                    Property.Value("Message", "My Message")
+                    && Property.Value("Severity", 100)
+                    && Property.Value("Source", "Any Source"));
+
+            }
+
+            mockService.LogError(new TraceMessage("My Message", 100, "Any Source"));
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void CompareObjectWithDelegateExample()
+        {
+            MockRepository mocks = new MockRepository();
+            IWebService mockService = mocks.CreateMock<IWebService>();
+
+            using (mocks.Record())
+            {
+                mockService.LogError(new TraceMessage("", 0, ""));
+
+                LastCall.Constraints(
+                    Is.Matching<TraceMessage>(
+                        delegate(TraceMessage msg)
+                        {
+                            if (msg.Severity < 50 && msg.Message.Contains("a"))
+                            {
+                                return false;
+                            }
+                            return true;
+                        }
+                        ));
+            }
+
+            mockService.LogError(new TraceMessage("My Message", 100, "Any Source"));
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void CompareObjectWithCallbacksExample()
+        {
+            MockRepository mocks = new MockRepository();
+            IWebService mockService = mocks.CreateMock<IWebService>();
+
+            using (mocks.Record())
+            {
+                mockService.LogError(new TraceMessage("", 0, ""));
+                LastCall.Constraints(Is.Matching<TraceMessage>(verifyComplexMessage));
+            }
+
+            mockService.LogError(new TraceMessage("My Message", 100, "Any Source"));
+            mocks.VerifyAll();
+        }
+
+        private bool verifyComplexMessage(TraceMessage msg)
+        {
+            if( msg.Severity < 50 && msg.Message.Contains("a") )
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ UnitTesting/SimpleMockExample.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Rhino.Mocks.Constraints;
+
+namespace UnitTesting
+{
+    public class TraceMessage
+    {
+        public string Message { get; set; }
+        public int Severity { get; set; }
+        public string Source { get; set; }
+
+        public TraceMessage(string _strMsg, int _iSeverity, string _strSource)
+        {
+            Message = _strMsg;
+            Severity = _iSeverity;
+            Source = _strSource;
+        }
+    }
+    public interface IWebService
+    {
+        void LogError(string _strMsg);
+        void LogError(TraceMessage _objTrace);
+    }
+
+    public class LogAnalyzer
+    {
+        private IWebService service;
+
+        public LogAnalyzer(IWebService service)
+        {
+            this.service = service;
+        }
+
+        public void Analyze(string fileName)
+        {
+            if (fileName.Length < 8)
+            {
+                service.LogError("Filename too short:"+fileName);
+            }
+        }
+    }
+
+    public interface IGetResults
+    {
+        int GetSomeNumber(string _strInput);
+    }
+    class SimpleMockExample
+    {
+        [Test]
+        public void SimpleRecodingExample()
+        {
+            MockRepository mocks = new MockRepository();
+            IWebService simulatedService = mocks.StrictMock<IWebService>();
+
+            using (mocks.Record())
+            {
+                simulatedService.LogError("Filename too short:abc.ext");
+            }
+
+            LogAnalyzer log = new LogAnalyzer(simulatedService);
+            string tooShortFileName = "abc.ext";
+            log.Analyze(tooShortFileName);
+
+            mocks.Verify(simulatedService);
+
+        }
+
+        [Test]
+        public void ConstraintWithStringMatchingExample()
+        {
+            MockRepository mocks = new MockRepository();
+            IWebService mockService = mocks.CreateMock<IWebService>();
+
+            using (mocks.Record())
+            {
+                mockService.LogError("Ignored String.");
+                LastCall.Constraints(new Rhino.Mocks.Constraints.Contains("abc"));
+            }
+            
+            mockService.LogError(Guid.NewGuid() + "abc");
+            mocks.VerifyAll();
+        }
+        [Test]
+        public void ReturnResultsFromStubExample()
+        {
+            MockRepository mocks = new MockRepository();
+            IGetResults resultGetter = mocks.Stub<IGetResults>();
+
+            using (mocks.Record())
+            {
+                resultGetter.GetSomeNumber("a");
+                LastCall.Return(1);
+            }
+
+            int result = resultGetter.GetSomeNumber("a");
+            Assert.AreEqual(1, result);
+
+        }
+
+    }
+}
